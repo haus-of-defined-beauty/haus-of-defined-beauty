553109fe4f9ce68a39845eb1d59d326887fcba91
thabogabe <gabethabo23@gmail.com>
2026-04-22 20:46:40 +0200

    feat: restore axios auth header from localStorage on app boot

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import axios from 'axios';
 import './index.css';
 import App from './App';
+
+const token = localStorage.getItem('token');
+if (token) axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
